cf8c99d919627463461626ddbbf725920893f2e6
Maximiliano Jofré <66652144+MaxiJ16@users.noreply.github.com>
2022-07-07 10:32:50 -0300

    Update footer.js

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -5,7 +5,7 @@ function footerComponent(el) {
     <h2 class="footer__title">Maxi</h2>
     <ul class="footer__social-link">
       <a href="https://www.instagram.com/maaxij/" class="social-link__a">Instagram <img class="social-link__img" src="./img/instagram.svg" alt="instagram"></a>
-      <a href="https://www.linkedin.com/in/maximiliano-jofr%C3%A9-0b7a781a8/" class="social-link__a">Linkedin <img class="social-link__img" src="./img/linkedin.svg" alt="linkedin"></a>
+      <a href="https://www.linkedin.com/in/maximiliano-jofre/" class="social-link__a">Linkedin <img class="social-link__img" src="./img/linkedin.svg" alt="linkedin"></a>
       <a href="https://github.com/MaxiJ16" class="social-link__a">Github <img class="social-link__img" src="./img/Vector (2).png" alt="github"></a>
     </ul>
   `;
